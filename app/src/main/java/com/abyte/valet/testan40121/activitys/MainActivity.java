2b683b56b1ba0cb2876bf184e3e77cdcbcf805c8
Valet-byte <75389128+Valet-byte@users.noreply.github.com>
2021-02-04 23:38:02 +0300

    git push -u origin main

--- app/src/main/java/com/abyte/valet/testan40121/activitys/MainActivity.java
+++ app/src/main/java/com/abyte/valet/testan40121/activitys/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//PPPPPPPPPPPPPPPPPPPPPPPPPOMAAAAAAAA
         projectFragment = new ProjectsFragment();
         ideaFragment = new IdeaFragment();
         articleFragment = new ArticleFragment();
